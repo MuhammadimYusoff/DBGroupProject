8f6e6c57847b66c348b81fccf7e21ec496ce1b47
muyusoffjamal <muhammadimyusoff@gmail.com>
2024-01-08 15:11:24 +0800

    add new files for each tasks

---
+++ SQL/Task_6.sql
@@ -0,0 +1,12 @@
+/*
+Write some SQL statements that allow you to query, add, modify and remove some data (CRUD Operation). 
+
+Examples: 
+•⁠  ⁠List all cars in the database
+•⁠  ⁠⁠List all rented cars
+•⁠  ⁠⁠Fine particular user of any car
+•⁠  ⁠⁠Allow to add, modify and remove some details of car rental
+•⁠  ⁠⁠Allow to add, modify and remove car renter
+
+*/
+
---
+++ SQL/Task_8.sql
@@ -0,0 +1,8 @@
+/*
+Write a script to create two functions. 
+Example:
+•⁠  ⁠Calculate how much renter have to pay for a certain period of renting a car
+•⁠  ⁠⁠Calculate anything from the table for the total of the calculation.
+Demonstrate the functions from anonymous block (refer Function slides)
+*/
+
