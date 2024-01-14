41924e4b5ab30e60f14e26d9d7cf8a5a679a71af
Muhammad Yusoff <muhammadimyusoff@gmail.com>
2024-01-14 19:35:21 +0800

    repush empty table and data separately

---
+++ SQL/CarRentalManagementSystem_Table.sql
@@ -0,0 +1,181 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 14, 2024 at 11:54 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `carrentalproject`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `ADMIN_ID` varchar(255) NOT NULL,
+  `ADMIN_PASSWORD` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `booking`
+--
+
+CREATE TABLE `booking` (
+  `BOOK_ID` int(11) NOT NULL,
+  `CAR_ID` int(11) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `BOOK_LOCATION` varchar(255) NOT NULL,
+  `BOOK_DATE` date NOT NULL,
+  `DURATION` int(11) NOT NULL,
+  `PHONE_NO` int(11) NOT NULL,
+  `DESTINATION` varchar(255) NOT NULL,
+  `RETURN_DATE` date NOT NULL,
+  `AMOUNT` double NOT NULL,
+  `STATUS` varchar(255) NOT NULL DEFAULT 'UNDER PROCESSING'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cars`
+--
+
+CREATE TABLE `cars` (
+  `CAR_ID` int(11) NOT NULL,
+  `CAR_NAME` varchar(255) NOT NULL,
+  `CAR_FUEL` varchar(255) NOT NULL,
+  `CAPACITY` int(11) NOT NULL,
+  `RATE_PER_DAY` double NOT NULL,
+  `CAR_IMG` varchar(255) NOT NULL,
+  `AVAILABILITY` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer`
+--
+
+CREATE TABLE `customer` (
+  `FNAME` varchar(255) NOT NULL,
+  `LNAME` varchar(255) NOT NULL,
+  `EMAIL` varchar(255) NOT NULL,
+  `LICENSE_NO` varchar(100) NOT NULL,
+  `PHONE_NO` int(11) NOT NULL,
+  `PASSWORD` varchar(255) NOT NULL,
+  `GENDER` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payment`
+--
+
+CREATE TABLE `payment` (
+  `PAY_ID` int(11) NOT NULL,
+  `BOOK_ID` int(11) NOT NULL,
+  `CARD_NO` varchar(100) NOT NULL,
+  `EXP_DATE` varchar(10) NOT NULL,
+  `CVV` int(11) NOT NULL,
+  `AMOUNT` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`ADMIN_ID`);
+
+--
+-- Indexes for table `booking`
+--
+ALTER TABLE `booking`
+  ADD PRIMARY KEY (`BOOK_ID`),
+  ADD KEY `CAR_ID` (`CAR_ID`),
+  ADD KEY `EMAIL` (`EMAIL`);
+
+--
+-- Indexes for table `cars`
+--
+ALTER TABLE `cars`
+  ADD PRIMARY KEY (`CAR_ID`);
+
+--
+-- Indexes for table `customer`
+--
+ALTER TABLE `customer`
+  ADD PRIMARY KEY (`EMAIL`);
+
+--
+-- Indexes for table `payment`
+--
+ALTER TABLE `payment`
+  ADD PRIMARY KEY (`PAY_ID`),
+  ADD UNIQUE KEY `BOOK_ID` (`BOOK_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `booking`
+--
+ALTER TABLE `booking`
+  MODIFY `BOOK_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=211;
+
+--
+-- AUTO_INCREMENT for table `cars`
+--
+ALTER TABLE `cars`
+  MODIFY `CAR_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=111;
+
+--
+-- AUTO_INCREMENT for table `payment`
+--
+ALTER TABLE `payment`
+  MODIFY `PAY_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=311;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `booking`
+--
+ALTER TABLE `booking`
+  ADD CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`CAR_ID`) REFERENCES `cars` (`CAR_ID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`EMAIL`) REFERENCES `customer` (`EMAIL`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `payment`
+--
+ALTER TABLE `payment`
+  ADD CONSTRAINT `payment_ibfk_1` FOREIGN KEY (`BOOK_ID`) REFERENCES `booking` (`BOOK_ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
